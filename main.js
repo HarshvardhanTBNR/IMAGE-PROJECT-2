e4a63d28bc5c9e0978a75d60afa0a275b6735628
HarshvardhanTBNR <76466857+HarshvardhanTBNR@users.noreply.github.com>
2021-09-03 16:53:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,17 +20,5 @@ Webcam.set({
     console.log("Model loaded")
     }
     
-    function identify(){
-    img=document.getElementById("captured_img");
-    classifier.classify(img,gotResult)
-    }
     
-    function gotResult(error,results){
-    if (error) {
-      console.error(error)  
-    } else {console.log(results);
-    document.getElementById("final_object").innerHTML=results[0].label;
-    document.getElementById("confidence").innerHTML=results[0].confidence.toFixed(2);
-    }
-    }
     
